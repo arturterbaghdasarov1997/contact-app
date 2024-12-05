8123a373c5bc7af351f3e93bdec9ab0bcda3b5a8
unknown <terbagdasarovartur@gmail.com>
2024-12-05 22:38:55 +0400

    Add contacts app

---
+++ app/(tabs)/Row.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+interface RowProps {
+  name: string;
+  phone: string;
+}
+
+const Row: React.FC<RowProps> = ({ name, phone }) => {
+  return (
+    <View style={styles.row}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.phone}>{phone}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    margin: 20,
+    width: '80%',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'blue',
+    alignSelf: 'center',
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  phone: {
+    fontSize: 16,
+    color: "#555",
+  },
+});
+
+export default Row;
